93d266848b2ac24bf8d1cc8850d49e1e1a528702
Maksym-Buriak <126103830+Maksym-Buriak@users.noreply.github.com>
2023-03-15 12:31:57 +0200

    Add files via upload

---
+++ Завдання 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Завдання_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> {"яблуко", "груша", "слива"};
+
+            var ListSum = list.Sum(a => a.Length);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"\nСума довжин усіх рядків = {ListSum}");
+
+            Console.ReadKey();
+        }
+    }
+}
